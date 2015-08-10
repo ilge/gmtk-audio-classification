3ddbad9ae45a54e7c6fd69e5bf024ecbe87a404b
ilgea <ilgea@eecs.berkeley.edu>
2015-08-10 03:25:08 -0700

    cleanup

--- afte4/offline_test.sh
+++
@@ -1,5 +0,0 @@
-obs-cat \
-  -fmt1 ascii \
-  -of1 testData.txt \
-  -binaryOutput F | \
-stream2json 5 0
--- decoding/applause.sh
+++ decoding/applause.sh
@@ -5,7 +5,7 @@ gmtkOnline \
   -os1 - -nf1 5 -fmt1 ascii \
   -strFile applause_detector.str \
   -inputMasterFile applause_detector.mtr \
-  -inputTrainable 4comp_20.gmp\
-  -numSmooth 10 -vitRunLength \
+  -inputTrainable 64comp_10.gmp\
+  -numSmooth 2 -vitRunLength \
   -viterbi -mVitValsFile - | \
 vit2json
