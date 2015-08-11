1b8fdcb75b974452babe6b16d6eb80966aa84da6
ilgea <ilgea@eecs.berkeley.edu>
2015-08-10 17:11:17 -0700

    changes for learning model

--- afte4/decoding/applause.sh
+++ afte4/decoding/applause.sh
@@ -5,6 +5,6 @@ gmtkOnline \
   -os1 - -nf1 25 -fmt1 ascii \
   -strFile applause_detector.str \
   -inputMasterFile applause_detector.mtr \
-  -inputTrainable ../training/learnedParams/64comp-frame.gmp\
+  -inputTrainable 64comp-frame.gmp\
   -numSmooth 3 -vitRunLength -viterbi -mVitValsFile - | \
 vit2json
