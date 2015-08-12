22b87c736b5e315bae755a27bc343f9967464355
ilgea <ilgea@eecs.berkeley.edu>
2015-08-11 21:24:12 -0700

    increased frame rate, fixed false alarms'

---
+++ afte6/decoding/applause4.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+json2stream 36 0 | \
+gmtkOnline \
+  -os1 - -nf1 36 -fmt1 ascii \
+  -strFile applause_detector.str \
+  -inputMasterFile applause_detector.mtr \
+  -inputTrainable 4comp-frame.gmp\
+  -numSmooth 1 -viterbi -mVitValsFile - | \
+vit2json
