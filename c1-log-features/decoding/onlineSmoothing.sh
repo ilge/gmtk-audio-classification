9833fb84c5654407404dd72d80e465e0cac73f2d
ilgea <ilgea@eecs.berkeley.edu>
2015-09-02 20:31:15 -0700

    logfeaturemodel

---
+++ c1-log-features/decoding/onlineSmoothing.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+json2stream 10 0 | \
+gmtkOnline \
+  -os1 - -nf1 10 -fmt1 ascii \
+  -strFile applause_detector.str \
+  -inputMasterFile applause_detector.mtr \
+  -inputTrainable $1\
+  -numSmooth 15 -viterbi -mVitValsFile - | \
+vit2json
