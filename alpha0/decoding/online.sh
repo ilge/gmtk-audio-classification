8b45261b52676de7be102833ca20a12983fbadac
ilgea <ilgea@eecs.berkeley.edu>
2015-09-05 01:58:26 -0700

    alpha0

---
+++ alpha0/decoding/online.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+json2stream 10 0 | \
+gmtkOnline \
+  -os1 - -nf1 10 -fmt1 ascii \
+  -strFile applause_detector.str \
+  -inputMasterFile applause_detector.mtr \
+  -inputTrainable $2\
+  -numSmooth $1 -viterbi -mVitValsFile - | \
+vit2json
